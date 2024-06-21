6f50e9ef7ff11dcf55794b0027ff2e3d82126385
Osama Alathwari <osamaz1x13@gmail.com>
2024-06-21 10:10:20 +0300

    Fixed api routes typos

--- developement/corecare_Srever/index.js
+++ developement/corecare_Srever/index.js
@@ -1,15 +1,18 @@
 // app.js
 import express from 'express';
-import bodyParser from 'body-parser';
+import cors from 'cors';
 import patientRoutes from './routes/patientRoutes.js';
 import loginRoutes from './routes/loginRoutes.js'
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use('/patient', patientRoutes);
-app.use('login', loginRoutes);
+app.use(cors());
+app.use(express.json());
+
+
+app.use('/patients', patientRoutes);
+app.use('/login', loginRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
