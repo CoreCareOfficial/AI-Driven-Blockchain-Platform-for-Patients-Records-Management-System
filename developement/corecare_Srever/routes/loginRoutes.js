3ce65fe6177106e48b96d05fb28ccc3b821c22e9
Osama Alathwari <osamaz1x13@gmail.com>
2024-06-22 11:59:45 +0300

    Added APIs for Register Accout, Login and Forgot Password

--- developement/corecare_Srever/routes/loginRoutes.js
+++ developement/corecare_Srever/routes/loginRoutes.js
@@ -1,6 +1,7 @@
 // loginRoutes.js
 import express from 'express';
 import pool from '../db.js';
+import bodyParser from 'body-parser';
 
 const router = express.Router();
 
@@ -19,5 +20,45 @@ router.post('/', async (req, res) => {
         res.status(500).send(err.message);
     }
 });
+
+// Register Account
+router.post('/add', async (req, res) => {
+    const { email, password, userType } = req.body
+    try {
+        const newLogin = await pool.query('INSERT INTO LOGIN (email, password, type) VALUES ($1, $2, $3) RETURNING *', [email, password, userType]);
+        res.json(newLogin.rows[0])
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// Login
+router.post('/get', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const checkLogin = await pool.query('SELECT type FROM LOGIN WHERE email = $1 and password = $2', [email, password]);
+        res.json(checkLogin.rows[0].type);
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
+// Forgot Password
+router.put('/', async (req, res) => {
+    const { email, newPassword } = req.body;
+    try {
+        const updatePassword = await pool.query('UPDATE LOGIN SET password = $1 WHERE email = $2 RETURNING *', [newPassword, email]);
+        if (updatePassword.rowCount === 0) {
+            return res.status(404).send('email not found');
+        }
+        res.status(200).send("Password Updated Successfully");
+
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+});
+
 
 export default router;
