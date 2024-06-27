02e640e807fed0d8fcc3c064bd25056f081f5593
MohammedMuthanna798380 <mohmd798380@gmail.com>
2024-06-27 10:13:06 +0300

    Fixsend profile page data to backend

--- developement/corecare/src/component/UserDetails/Profile.js
+++ developement/corecare/src/component/UserDetails/Profile.js
@@ -3,12 +3,18 @@ import defaultPic from '../../assets/user_signup.png'
 import ProfileHeader from "./ProfileHeader";
 import ProfileBody from "./ProfileBody";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
+import { loginInfo } from "../../Recoil/Atom";
+import { useRecoilValue } from "recoil";
 
 function Profile(props) {
+    const loginInfoValue = useRecoilValue(loginInfo);
     const [patient, setPatient] = useState(null);
+    const [userInfo, setUserInfo] = useState({});
     const [error, setError] = useState(null);
+    const hasEffectRun = useRef(false);
+
 
     let userType = '';
     if (props.userType !== "Patient" && props.userType !== "Doctor") {
@@ -21,34 +27,58 @@ function Profile(props) {
 
     console.log(userType);
 
+    const getUserData = async (fetchText, param) => {
+        console.log(param);
+        try {
+            const response = await fetch(fetchText, {
+                body: param
+            });
+            const jsonData = await response.json();
+            setUserInfo(jsonData);
+            console.log('succes');
+        } catch (err) {
+            setError(err.response ? err.response.data : err.message);
+            console.error('Error:', err);
+        }
+    };
+
     useEffect(() => {
-        const fetchPatientData = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5000/${userType}`, {
-                    params: { email: 'osamaz1x13@gmail.com' }
-                });
-                setPatient(response.data);
-            } catch (err) {
-                setError(err.response ? err.response.data : err.message);
-                console.error('Error:', err);
-            }
-        };
+        if (!hasEffectRun.current) {
+            getUserData(`http://localhost:5000/${userType}`, { email: loginInfoValue.login });
+            console.log("user info: " + userInfo);
+            console.log(userInfo);
+            // const fetchPatientData = async () => {
+            //     console.log(loginInfoValue.login);
+            //     try {
+            //         const response = await axios.get(`http://localhost:5000/${userType}`, {
+            //             params: { email: loginInfoValue.login }
+            //         });
+            //         setPatient(response.data);
+            //     } catch (err) {
+            //         setError(err.response ? err.response.data : err.message);
+            //         console.error('Error:', err);
+            //     }
+            // };
 
-        // if (patient.patientid) {
-        //     try {
-        //         const response = await axios.get(`http://localhost:5000/socialmedia`, {
-        //             params: {patientid: patient.patientid}
-        //         });
-        //     } catch (err) {
-        //         setError(err.response ? err.response.data : err.message);
-        //         console.error('Error:', err);
-        //     }
-        // }
 
-        fetchPatientData();
+            // if (patient.patientid) {
+            //     try {
+            //         const response = await axios.get(`http://localhost:5000/socialmedia`, {
+            //             params: {patientid: patient.patientid}
+            //         });
+            //     } catch (err) {
+            //         setError(err.response ? err.response.data : err.message);
+            //         console.error('Error:', err);
+            //     }
+            // }
+
+            // fetchPatientData();
+            hasEffectRun.current = true;
+        }
     }, []);
 
-    console.log('patient:', patient);
+    console.log('patient:', userInfo);
+    console.log(userInfo);
 
     if (error) {
         return <div>Error: {error}</div>;
