dddd7600f1eca60fb09eda191d00a44bf04646a6
Osama Alathwari <osamaz1x13@gmail.com>
2024-07-27 18:41:36 +0300

    Connecting the server with the deployed database

--- developement/corecare_Srever/db.js
+++ developement/corecare_Srever/db.js
@@ -2,9 +2,9 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "osama",
-    host: "localhost",
+    user: "root",
+    password: "CK5ubY7iZitsWUMQx3pkrEd4TXXTUsOI",
+    host: "postgresql://root:CK5ubY7iZitsWUMQx3pkrEd4TXXTUsOI@dpg-cqefu4ggph6c73ao970g-a/corecareofficial",
     port: 5432,
     database: "corecareofficial"
 });
